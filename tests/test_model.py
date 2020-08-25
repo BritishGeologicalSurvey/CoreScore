86ce08832c13eedcc5e37492e82edb9d5a3152f5
Jo Walsh <jowalsh@bgs.ac.uk>
2020-08-25 15:28:00 +0100

    test which fastai version we have

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,7 +1,12 @@
 import os
+import fastai
 from corescore.models import CoreModel
 
 
 def test_create_model():
     model = CoreModel(os.getcwd())
 
+
+def test_version():
+    major = fastai.__version__.split('.')[0]
+    assert int(major) > 1
