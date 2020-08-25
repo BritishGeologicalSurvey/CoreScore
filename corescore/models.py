3846106b4df0761cefd25ff057b41f8e13571118
Jo Walsh <jowalsh@bgs.ac.uk>
2020-08-25 16:22:47 +0100

    fix lots of the references, but not all

--- corescore/models.py
+++ corescore/models.py
@@ -35,21 +35,22 @@ class CoreModel():
         self.wd = wd
         self.pct_start = pct_start
         self.epochs = epochs
+        self.reduce_size = np.array([160, 1048]) # TODO derive from samples
 
 #src_size / 6
 #array([ 148., 1048.])
     def image_src(self):
-        self.src = (SegmentationItemList.from_folder(path_img).split_by_rand_pct().label_from_func(get_y_fn, classes=LABELS))
+        self.src = (SegmentationItemList.from_folder(self.path_img).split_by_rand_pct().label_from_func(self.get_y_fn, classes=LABELS))
         self.src.train.y.create_func = partial(open_mask, div=True)
         self.src.valid.y.create_func = partial(open_mask, div=True)
         return self.src
 
     def image_data(self):
         self.data = self.image_src().transform(get_transforms(),
-                                               size=reduce_size,
+                                               size=self.reduce_size,
                                                tfm_y=True)
-        self.data.databunch(bs=self.batch_size, num_workers=0) # set 0 to avoid ForkingPickler pipe error in Windows
-        self.data.normalize(imagenet_stats)
+        bunch = self.data.databunch(bs=self.batch_size, num_workers=0) # set 0 to avoid ForkingPickler pipe error in Windows
+        bunch.normalize(imagenet_stats)
 
     def learner(self):
         def acc_rock(input, target):
@@ -58,7 +59,7 @@ class CoreModel():
             return (input.argmax(dim=1)[mask]==target[mask]).float().mean()
 
         metrics = acc_rock
-        self.learn = unet_learner(data, models.resnet34, metrics=metrics, wd=self.wd)
+        self.learn = unet_learner(self.image_data(), models.resnet34, metrics=metrics, wd=self.wd)
         self.learn.model = torch.nn.DataParallel(learn.model)
         self.learn.lr_find()
 
@@ -68,6 +69,9 @@ class CoreModel():
         self.learner().fit_one_cycle(self.epochs,
                                      slice(lr),
                                      pct_start=self.pct_start)
+
+    def get_y_fn(self, x):
+        return self.path_lbl/f'{x.stem}{x.suffix}'
 
     def save(self):
         # TODO save via MLFLow
