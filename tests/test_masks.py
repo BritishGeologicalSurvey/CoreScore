67dedce01d5890763f09f760c788b323ce8bf7fe
vyronc <vyronc@bgs.ac.uk>
2020-09-01 12:03:00 +0000

    Revert "Add failing assertion to mask test"
    This reverts commit 5319bcc6aa50ec876ba3198dd29b4d71a146c978.

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -2,7 +2,7 @@ import os
 import pytest
 from skimage.io import imread
 from corescore.masks import CoreImageProcessor
-import numpy as np
+
 
 @pytest.fixture
 def image_dir():
@@ -28,9 +28,7 @@ def test_load_processor(image_dir, labels):
         mask_file = coreProcessor.processImage(image)
         im_arr = imread(mask_file).flatten()
         has_data = im_arr.any()
-        print(np.shape(im_arr))
         # Test if mask is blank
         assert has_data
-        assert np.count_nonzero(im_arr==0) == np.shape(im_arr)[0] 
         if has_data: total += 1
     assert total > 1
