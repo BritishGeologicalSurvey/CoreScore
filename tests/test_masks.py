c128c912e15f0737822e6cba46080d87c4e0ba9a
Jo Walsh <jowalsh@bgs.ac.uk>
2020-09-02 13:07:19 +0100

    make masks from either LabelBox or LabelTool exports
    * test both the cropped core and the corebreakout images

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -19,10 +19,32 @@ def labels():
         'fixtures', 'labels.json')
 
 
+@pytest.fixture
+def labeltool_labels():
+    return os.path.join(
+        os.path.dirname(os.path.abspath(__file__)),
+        'fixtures', 'labeltool.json')
+
 def test_load_processor(image_dir, labels):
     coreProcessor = CoreImageProcessor(image_dir, labels=labels)
-    # TODO store/move labels differently
     assert len(coreProcessor.core_types)
+    total = 0
+    for image in coreProcessor.core_types:
+        mask_file = coreProcessor.processImage(image)
+        im_arr = imread(mask_file).flatten()
+        has_data = im_arr.any()
+        # Test if mask is blank
+        assert has_data
+        if has_data:
+            total += 1
+    assert total > 1
+
+
+def test_load_labeltool(image_dir, labeltool_labels):
+    coreProcessor = CoreImageProcessor(image_dir, labels=labeltool_labels)
+    assert len(coreProcessor.core_types)
+    print(coreProcessor.core_types)
+
     total = 0
     for image in coreProcessor.core_types:
         mask_file = coreProcessor.processImage(image)
