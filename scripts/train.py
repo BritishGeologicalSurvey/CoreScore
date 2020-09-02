e1a4ca3768927265b93bda3bfca3f4de415c4b73
vyronc <vyronc@bgs.ac.uk>
2020-09-02 10:20:22 +0000

    Move mlflow logging in CoreModel class

--- scripts/train.py
+++ scripts/train.py
@@ -1,15 +1,20 @@
 import argparse
 import os
-from corescore.models import CoreModel
 
-def train(epochs=10, path=os.getcwd()):
+from corescore.models import CoreModel
+import mlflow
+
+
+def train(epochs=10, lr=0.00001, path=os.getcwd()):
     coremodel = CoreModel(path, epochs=epochs)
-    coremodel.fit()
+    coremodel.fit(lr=lr)
     coremodel.save()
+        
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--epochs', help="Epochs to train, default 10")
+    parser.add_argument('--lr', help="Learning rate to perform training")
     args = parser.parse_args()
-    train(epochs=args.epochs)
+    train(epochs=int(args.epochs), lr=float(args.lr))
 
