7b914d02af804bb8ec572f4a1bcb49249087e85c
Jo Walsh <jowalsh@bgs.ac.uk>
2020-09-08 01:16:49 +0100

    load latest model by name (without registry)

--- scripts/example_query_mlflow.py
+++ scripts/example_query_mlflow.py
@@ -1,15 +1,26 @@
 import os
-
 import argparse
 
+import mlflow
+
 from corescore.mlflowregistry import MlflowRegistry
+
 
 def list_models(client):
     for rm in client.list_registered_models():
         print(rm)
 
+
+def load_latest(client, name):
+    models = client.list_registered_models()
+    models = list(filter(lambda model: model.name == name, models))
+    latest = sorted(models, key=lambda model: model.latest_versions[0].version)[0]
+    mlflow.fastai.load_model(latest.latest_versions[0].source)
+
+
 def register_model(client, tag, search_str):
     client.register_latest(tag, search_str)
+
 
 if __name__ == '__main__':
     URI = os.environ.get('MLFLOW_TRACKING_URI', '')
@@ -17,10 +28,13 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--tag', type=str, help="model tag")
     parser.add_argument('--name', type=str, help="model name")
+    parser.add_argument('--load', type=str, help="load model")
     args = parser.parse_args()
 
     if args.tag and args.name:
         register_model(client, tag=args.tag, search_str=args.name)
+    elif args.name and args.load:
+        load_latest(client, name=args.name)
     else:
         list_models(client)
 
