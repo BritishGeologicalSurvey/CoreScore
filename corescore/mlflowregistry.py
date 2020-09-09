a4c5da3c4907fb0194c6c67be9803a51088febfd
vyronc <vyronc@bgs.ac.uk>
2020-09-09 10:27:03 +0000

    - Lose register_best, get_best functions - Add function that returns a list of logged experiments - Change behaviour so it always registers the latest model but only if it's better than the previous.

--- corescore/mlflowregistry.py
+++ corescore/mlflowregistry.py
@@ -13,41 +13,28 @@ class MlflowRegistry(MlflowClient):
 
     def get_info(self, search_result):
         """Given a search response from the mlflow api
-           return artifact_uri and run_id"""
+           return artifact_uri, run_id and metrics"""
         search_dict = search_result.to_dictionary()
         artifact_uri = search_dict['info']['artifact_uri']
         run_id = search_dict['info']['run_id']
-        return {'artifact_uri': artifact_uri, 'run_id': run_id}
+        metrics = search_dict['data']['metrics']
+        return {'artifact_uri': artifact_uri, 'run_id': run_id, 'metrics': metrics}
 
-    def get_best(self, tag, search_str, metric):
-        """Query the mlflow api and return
-            best model"""
-        query = f"tags.{tag} = '{search_str}'"
-        search_result = self.client.search_runs(experiment_ids="0",
-                                                filter_string=query,
-                                                order_by=[f"metrics.{metric} DESC"])[0]
-        return self.get_info(search_result)
-
-    def get_latest(self, tag, search_str):
-        """Query the mlflow api and return
-           latest model"""
+    def list_experiments(self, tag, search_str):
+        """Query the mlflow api and list
+           logged models"""
         query = f"tags.{tag} = '{search_str}'" 
-        search_result = self.client.search_runs(experiment_ids="0",
-                                                filter_string=query)[0]
-        return self.get_info(search_result)
-
-    def register_best(self, tag, search_str, metric, name="best-reg-model"):
-        """Register best model"""
-        best_model = self.get_best(tag=tag,
-		        	   search_str=search_str,
-                                   metric=metric)
-        mlflow.register_model(best_model['artifact_uri'],
-                              name)
-
-
-    def register_latest(self, tag, search_str, name="latest-reg-model"):
-        """Register latest model"""
-        latest_model = self.get_latest(tag=tag,
+        return self.client.search_runs(experiment_ids="0",
+                                       filter_string=query)
+ 
+    def register_model(self, tag, search_str, metric, override=False, name="latest-reg-model"):
+        """Register a model if it shows better performance"""
+        exp_list = self.list_experiments(tag=tag,
                                        search_str=search_str)
-        mlflow.register_model(latest_model['artifact_uri'],
+        old_metric = self.get_info(exp_list[1])['metrics'][f"{metric}"]
+        latest_metric = self.get_info(exp_list[0])['metrics'][f"{metric}"]
+        if old_metric >= latest_metric and not override:
+            print(f"Latest model's {metric} is {latest_metric} and not better than {old_metric}. Set `override=True` to override. Model was not registered.")
+        else:
+            mlflow.register_model(self.get_info(exp_list[0])['artifact_uri'],
                               name)
